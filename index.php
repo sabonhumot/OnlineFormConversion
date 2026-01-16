a9e286c705230482322d2051701a254eed22017c
sabonhumot <arljoshua9@gmail.com>
2026-01-16 10:46:21 +0800

    lacking other fields

--- index.php
+++ index.php
@@ -13,74 +13,131 @@
     <div class="container">
     <h1>Online Form</h1>
     <form id = "onlineForm" onsubmit="return validateForm()" method="post" action="submit.php">
-        <h2>General Information</h2>
+        <h2>Personal Data</h2>
         <div class="gen-info">
-            <div class="name-field">
-                <label for="firstName">First Name</label>
-                <input type="text" id="firstName" name="fname">
-                <span id="fnameError" class="error"></span>
+            <div class="name-row">
+                <div class="input-field">
+                    <label for="firstName">First Name</label>
+                    <input type="text" id="firstName" name="fname">
+                    <span id="fnameError" class="error"></span>
+                </div>
+                <div class="input-field">
+                    <label for="middleName">Middle Name</label>
+                    <input type="text" id="middleName" name="mname">
+                    <span id="mnameError" class="error"></span>
+                </div>
+                <div class="input-field">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" id="lastName" name="lname">
+                    <span id="lnameError" class="error"></span>
+                </div>
+                <div class="input-field">
+                    <label for="suffix">Suffix</label>
+                    <input type="text" id="suffix" name="suffix">
+                    <span id="suffixError" class="error"></span>
+                </div>
             </div>
-            <div class="name-field">
-                <label for="middleName">Middle Name</label>
-                <input type="text" id="middleName" name="mname">
-                <span id="mnameError" class="error"></span>
-            </div>
-            <div class="name-field">
-                <label for="lastName">Last Name</label>
-                <input type="text" id="lastName" name="lname">
-                <span id="lnameError" class="error"></span>
-            </div>
-            <div class="name-field">
-                <label for="dob">Date of Birth</label><br>
-                <input type="date" id="dob" name="dob"><br>
-                <span id="dobError" class="error"></span><br>
-            </div>
-            <div class="name-field">
-                <label for="sex">Sex</label><br>
-                <select id="sex" name="sex">
-                <option value="">--Select--</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                </select><br>
-                <span id="sexError" class="error"></span><br>
-            </div>
-            <div class="name-field">
-                <label for="civilStatus">Civil Status</label><br>
-                <select id="civilStatus" name="civilStatus">
-                <option value="">--Select--</option>
-                <option value="single">Single</option>
-                <option value="married">Married</option>
-                <option value="widowed">Widowed</option>
-                <option value="separated">Separated</option>
-                <option value="divorced">Divorced</option>
-                </select><br>
-                <span id="civilStatusError" class="error"></span><br>
+
+            <div class="other-info">
+                <div class="input-field">
+                    <label for="dob">Date of Birth</label>
+                    <input type="date" id="dob" name="dob"><br>
+                    <span id="dobError" class="error"></span><br>
+                </div>
+                <div class="input-field">
+                    <label for="sex">Sex</label>
+                    <select id="sex" name="sex">
+                    <option value="">--Select--</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    </select><br>
+                    <span id="sexError" class="error"></span><br>
+                </div>
+                <div class="input-field">
+                    <label for="civilStatus">Civil Status</label>
+                    <select id="civilStatus" name="civilStatus">
+                    <option value="">--Select--</option>
+                    <option value="single">Single</option>
+                    <option value="married">Married</option>
+                    <option value="widowed">Widowed</option>
+                    <option value="separated">Separated</option>
+                    <option value="divorced">Divorced</option>
+                    </select><br>
+                    <span id="civilStatusError" class="error"></span><br>
+                </div>
+                <div class="input-field">
+                    <label for="nationality">Nationality</label>
+                    <input type="text" id="nationality" name="nationality"><br>
+                    <span id="nationalityError" class="error"></span><br>
+                </div>
             </div>
         </div>  
 
-        <label for="nationality">Nationality</label><br>
-        <input type="text" id="nationality" name="nationality"><br>
-        <span id="nationalityError" class="error"></span><br>
-        <label for="pofbirth">Place of Birth</label><br>
-        <input type="text" id="pofbirth" name="pofbirth"><br>
-        <span id="pofbirthError" class="error"></span><br>
-        <label for="sameAdd">Same as Home Address</label>
-        <input type="checkbox" id="sameAdd" name="sameAdd"><br>
-        <label for="homeAdd">Home Address</label><br>
-        <input type="text" id="homeAdd" name="homeAdd"><br>
-        <span id="homeAddError" class="error"></span><br>
+        <div class="address-info">
+            <div class="input-field">
+                <label for="pofbirth">Place of Birth</label>
+                <input type="text" id="pofbirth" name="pofbirth"><br>
+                <span id="pofbirthError" class="error"></span><br>
+            </div>
+        
+            <div class="input-field">
+                <label for="homeAdd">Home Address</label>
+                <input type="text" id="homeAdd" name="homeAdd"><br>
+                <span id="homeAddError" class="error"></span><br>
+            </div>
+        </div>
 
-        <h2>Contact Information</h2>
-        <label for="pNum">Phone Number</label><br>
-        <input type="pNum" id="pNum" name="pNum"><br>
-        <span id="pNumError" class="error"></span><br>
-        <label for="emailAdd">Email Address</label><br>
-        <input type="email" id="emailAdd" name="emailAdd"><br>
-        <span id="emailAddError" class="error"></span><br>
+        <div class="checkbox-container">
+                <input type="checkbox" id="sameAdd" name="sameAdd">Same as Home Address
+            </div>
+  
+
+        <div class="input-field">
+            <label for="pNum">Phone Number</label>
+            <input type="pNum" id="pNum" name="pNum"><br>
+            <span id="pNumError" class="error"></span><br>
+        </div>
+        <div class="input-field">
+            <label for="emailAdd">Email Address</label>
+            <input type="email" id="emailAdd" name="emailAdd"><br>
+            <span id="emailAddError" class="error"></span><br>
+        </div>
+
+        
+        <div class="name-row">
+                <div class="input-field">
+                    <label for="firstName">Father's Name</label>
+                    <input type="text" id="firstName" name="fname">
+                    <span id="fnameError" class="error"></span>
+                </div>
+                <div class="input-field">
+                    <label for="dob">Date of Birth</label>
+                    <input type="date" id="dob" name="dob"><br>
+                    <span id="dobError" class="error"></span><br>
+                </div>
+        </div>
+                
+            <div class="name-row">
+                <div class="input-field">
+                    <label for="firstName">Mother's Maiden Name</label>
+                    <input type="text" id="firstName" name="fname">
+                    <span id="fnameError" class="error"></span>
+                </div>
+                <div class="input-field">
+                    <label for="dob">Date of Birth</label>
+                    <input type="date" id="dob" name="dob"><br>
+                    <span id="dobError" class="error"></span><br>
+                </div>
+            </div>
+            
 
         <input type="submit" id ="submitButton" value="Submit">
+
+        
     </form>
     </div>
+
+    
 
 
 
